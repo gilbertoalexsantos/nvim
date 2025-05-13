348e08df48f2da053c4b206a0baeb480d2762eda
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2025-05-13 13:14:12 +0200

    opa2

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -362,6 +362,7 @@ return {
       local packages = {
         "lua-language-server",
         "roslyn",
+        "ruby-lsp",
       }
 
       for _, pkg in ipairs(packages) do
@@ -387,6 +388,53 @@ return {
             "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
             "--stdio",
         },
+    },
+  },
+  {
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      main = "ibl",
+      lazy = false,
+      config = function()
+        require("ibl").setup()
+      end,
+    },
+    {
+      {
+        "lewis6991/gitsigns.nvim",
+        main = "gitsigns",
+        lazy = false,
+        config = function()
+          require('gitsigns').setup({
+            on_attach = function(bufnr)
+              local gitsigns = require('gitsigns')
+
+              local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+              end
+
+              -- Navigation
+              map('n', ']c', function()
+                if vim.wo.diff then
+                  vim.cmd.normal({']c', bang = true})
+                else
+                  gitsigns.nav_hunk('next')
+                end
+              end)
+
+              map('n', '[c', function()
+                if vim.wo.diff then
+                  vim.cmd.normal({'[c', bang = true})
+                else
+                  gitsigns.nav_hunk('prev')
+                end
+              end)
+            end
+          })
+        end,
+      }
     },
   }
 }
